7ebb1b21e2e5b806c83cb44f50579d735875fa4b
Arunav Bhattacharya <itsarunav4u@gmail.com>
2020-06-04 12:46:19 -0700

    feat(vidly-app) : sort-icon & refactored moviesTable

--- 3-vidly-app/src/components/moviesTable.jsx
+++ 3-vidly-app/src/components/moviesTable.jsx
@@ -1,43 +1,47 @@
 import React, { Component } from "react";
 import Like from "./common/like";
+import TableHeader from "./common/tableHeader";
+import TableBody from "./common/tableBody";
 
 class MoviesTable extends Component {
+  columns = [
+    { path: "title", label: "Title" },
+    { path: "genre.name", label: "Genre" },
+    { path: "numberInStock", label: "Stock" },
+    { path: "dailyRentalRate", label: "Rate" },
+    {
+      key: "like",
+      content: (movie) => (
+        <Like
+          liked={movie.liked}
+          onLike={() => this.props.onLike(movie)}
+        ></Like>
+      ),
+    },
+    {
+      key: "delete",
+      content: (movie) => (
+        <button
+          onClick={() => this.props.onDelete(movie)}
+          className="btn btn-danger"
+        >
+          Delete
+        </button>
+      ),
+    },
+  ];
+
   render() {
-    const { movies, onLike, onDelete, onSort } = this.props;
+    const { movies, onSort, sortColumn } = this.props;
 
     return (
       <table className="table">
-        <thead>
-          <tr>
-            <th onClick={() => onSort("title")}>Title</th>
-            <th onClick={() => onSort("genre.name")}>Genre</th>
-            <th onClick={() => onSort("numberInStock")}>Stock</th>
-            <th onClick={() => onSort("dailyRentalRate")}>Rate</th>
-            <th onClick={() => onSort()}></th>
-            <th onClick={() => onSort()}></th>
-          </tr>
-        </thead>
-        <tbody>
-          {movies.map((movie) => (
-            <tr key={movie._id}>
-              <td>{movie.title}</td>
-              <td>{movie.genre.name}</td>
-              <td>{movie.numberInStock}</td>
-              <td>{movie.dailyRentalRate}</td>
-              <td>
-                <Like liked={movie.liked} onLike={() => onLike(movie)}></Like>
-              </td>
-              <td>
-                <button
-                  onClick={() => onDelete(movie)}
-                  className="btn btn-danger"
-                >
-                  Delete
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
+        <TableHeader
+          columns={this.columns}
+          sortColumn={sortColumn}
+          onSort={onSort}
+        ></TableHeader>
+        <TableBody rows={movies} columns={this.columns} />
       </table>
     );
   }
